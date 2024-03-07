f0971590d60d38ad8fc2ada9c5c8b0cea704ace8
tjm2285 <tjm2285@yahoo.com>
2024-03-07 15:43:48 -0800

    created new shader graph material

---
+++ Explosive_Ordinance/Assets/Scripts/GridVisualization.cs
@@ -0,0 +1,49 @@
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+using static Unity.Mathematics.math;
+
+public struct GridVisualizations
+{
+    static int
+        positionsId = Shader.PropertyToID("_Positions"),
+        colorsId = Shader.PropertyToID("_Colors");
+
+    ComputeBuffer positionsBuffer, colorsBuffer;
+
+    NativeArray<float3> positions, colors;
+
+    Grid grid;
+
+    Material material;
+
+    Mesh mesh;
+
+    public void Initialize(Grid grid, Material material, Mesh mesh)
+    {
+        this.grid = grid;
+        this.material = material;
+        this.mesh = mesh;
+
+        int instanceCount = grid.CellCount;
+        positions = new NativeArray<float3>(instanceCount, Allocator.Persistent);
+        colors = new NativeArray<float3>(instanceCount, Allocator.Persistent);
+
+        positionsBuffer = new ComputeBuffer(instanceCount, 3 * 4);
+        colorsBuffer = new ComputeBuffer(instanceCount, 3 * 4);
+        material.SetBuffer(positionsId, positionsBuffer);
+        material.SetBuffer(colorsId, colorsBuffer);
+    }
+
+    public void Dispose()
+    {
+        positions.Dispose();
+        colors.Dispose(); 
+        positionsBuffer.Release();
+        colorsBuffer.Release();
+    }
+
+    public void Draw() => Graphics.DrawMeshInstancedProcedural(mesh, 0, material, new Bounds(Vector3.zero, Vector3.one), positionsBuffer.count);
+}
